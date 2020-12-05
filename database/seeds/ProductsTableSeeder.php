bec655ce76d8227d2b48f01b1e9744e4abd72573
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2020-12-05 21:16:25 +0100

    ss

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -22,7 +22,7 @@ class ProductsTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'name' => 'Grand Theft Auto V',
-                'image_name' => '1.jpg',
+                'image' => 'uploads/1.jpg',
                 'description' => "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the first main entry in the Grand Theft Auto series since 2008's Grand Theft Auto IV",
                 'price' => 70,
                 'discount' => 65,
@@ -34,7 +34,7 @@ class ProductsTableSeeder extends Seeder
             array (
                 'id' => 2,
                 'name' => 'FIFA 21',
-                'image_name' => '2.jpg',
+                'image' => 'uploads/2.jpg',
                 'description' => 'FIFA 21 is a football simulation video game published by Electronic Arts as part of the FIFA series. It is the 28th installment in the FIFA series, and was released 9 October 2020 for Microsoft Windows, Nintendo Switch, PlayStation 4 and Xbox One.',
                 'price' => 135,
                 'discount' => 130,
