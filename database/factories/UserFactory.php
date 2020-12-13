40b57164918108465f77c6221d8c67b6e1d9f704
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2020-12-13 22:48:32 +0100

    UnitTest of signup form

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'full_name' => $faker->name,
+        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'phone' => '123456',
-        'address_id' => 1,
+        'remember_token' => str_random(10),
     ];
 });
