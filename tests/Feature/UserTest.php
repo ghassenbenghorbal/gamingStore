559adc14ab6dee9ad2cc8e95c785ef9ad43212da
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2020-12-13 23:04:15 +0100

    signup unitTest

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function only_logged_in_users_can_access_settings(){
+        $response =$this->get('/settings')
+            ->assertRedirect('/login');
+    }
+    /** @test */
+    public function user_register_through_signup_form(){
+        $this->withoutExceptionHandling();
+        $response = $this->post('/signup', $this->getData());
+        $this->assertCount(1, User::all());
+    }
+
+    /** @test */
+    public function name_is_required_in_signup_form(){
+        $response = $this->post('/signup', array_merge($this->getData(), ['name'=>'']));
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, User::all());
+    }
+    /** @test */
+    public function password_is_at_least_8_characters(){
+        $response = $this->post('/signup', array_merge($this->getData(), ['password'=>'123', 'confirm_password' => '123']));
+        $this->assertCount(0, User::all());
+    }
+    public function getData(){
+        return [
+            'name' => 'achraf',
+            'email'=>'test@email.com',
+            'address' => 'test',
+            'city' => 'Rades',
+            'zip' => '4080',
+            'tel' => '222222',
+            'pass' => '369852147A&',
+            'confirm_password' => '369852147A&'
+        ];
+    }
+}
