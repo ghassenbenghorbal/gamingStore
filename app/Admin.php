be7db55f8c74e86ebda45ba81a77b72aa8d66832
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2021-01-03 21:03:59 +0100

    smthing fixed but idk what

--- app/Admin.php
+++ app/Admin.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class Admin extends Model
+class Admin extends Model implements Authenticatable
 {
+    use AuthenticableTrait;
     /**
      * The attributes that are mass assignable.
      *
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -6,6 +6,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Product;
+use Illuminate\Support\Facades\Hash;
+use App\Admin;
+use Carbon\Carbon;
 class ProductTest extends TestCase
 {
     use RefreshDatabase;
@@ -13,7 +16,26 @@ class ProductTest extends TestCase
     /** @test */
     public function new_products_are_visible()
     {
-        $product = factory(Product::class)->make();
-        $this->get('/search?n='.$product->name)->assertSee($product->name);
+        Product::create([
+            'name' => "product 1",
+            'image' => 'uploads/6.jpg',
+            'description' => 'published by Ubisoft. It is the twelfth major installment and the twenty-second release in the Assassin\'s Creed series, and a successor to the 2018 game Assassin\'s Creed Odyssey.',
+            'price' => 160,
+            'genre' => 'Action',
+            'discount' => null,
+            'tag' => 'HOT',
+            'category_id' => 3,
+            'created_at' => Carbon::now()
+        ]);
+        $admin = Admin::create([
+            'username' => 'admin',
+            'name' => 'Administrator',
+            'password' => Hash::make('admin'),
+            'created_at' => Carbon::now(),
+        ]);
+        $this->actingAs($admin);
+        $this->assertCount(1, Product::all());
+        $response = $this->get("/admin_panel/products");
+        $response->assertSee('product 2');
     }
 }
